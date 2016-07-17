8103e5dd8c321400574eb51c6fffbf161ade7ec3
Kyle Orlinski <orlinskikyle@gmail.com>
2016-07-17 15:51:52 -0500

    Update cave_level1.js

--- javascript/cave_level1.js
+++ javascript/cave_level1.js
@@ -55,70 +55,29 @@ var mapCave_level1 =[3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,3,3,
         };
 
 function cave_level1() {
+    //console.log(player_coordinates_x + "X");
+    //console.log(player_coordinates_y + "Y");
     requestID = requestAnimationFrame(cave_level1);
-    if (playerHealth <= 0) { window.location.reload();}
     drawCave_level1();
     Context.context.beginPath(); Context.context.fillStyle = "rgba(0,0,0,0.2)"; Context.context.fillRect(0,0,Context.width, Context.height); Context.context.fill();Context.context.closePath();
     resetAnimationCounter();
     playerMovement();
     is_playerMove = false;
     playerDirection = 0;
-    if (key.escape) {
-        return;    
-    }
-    if (player_coordinates_x + 20 > shadewalker1_x &&
-               player_coordinates_x < shadewalker1_x + shadeWalker_Sprite.image.width &&
-               player_coordinates_y + 10 > shadewalker1_y &&
-               player_coordinates_y < shadewalker1_y + shadeWalker_Sprite.image.height && shadewalker1_Alive == true) {
-                cancelAnimationFrame(requestID);
-                is_playerMove = false; attackShow = true; attackDisabled = false; is_slimeMove = false; attackSequence = 0; shadewalker1_Engaged = true; battleScreen = true; drawBattle();return;
-            }
-        if (player_coordinates_x + 20 > shadewalker2_x &&
-               player_coordinates_x < shadewalker2_x + shadeWalker_Sprite.image.width &&
-               player_coordinates_y + 10 > shadewalker2_y &&
-               player_coordinates_y < shadewalker2_y + shadeWalker_Sprite.image.height && shadewalker2_Alive == true) {
-                cancelAnimationFrame(requestID);
-                is_playerMove = false; attackShow = true; attackDisabled = false; is_slimeMove = false; attackSequence = 0; shadewalker2_Engaged = true; battleScreen = true; drawBattle();return;
-            }
+    if (key.escape) {return;} //Access Player Menu
     
-    if(shadewalker1_Alive) {
-        
-                if (shadewalker1_x >= 130 && shadewalker1_Right) {
-                    shadewalker1_x += 1;
-                    shadeWalker_Sprite.draw(shadewalker1_x, shadewalker1_y, [6,7,8]);
-                    if(shadewalker1_x >= 200) {
-                        shadewalker1_Right = false;
-                        shadewalker1_Left = true;
-                    }
-                }
-                if (shadewalker1_x <= 210 && shadewalker1_Left) {
-                    shadewalker1_x -= 1;
-                    shadeWalker_Sprite.draw(shadewalker1_x, shadewalker1_y, [3,4,5]);
-                    if (shadewalker1_x <= 130) {
-                        shadewalker1_Left = false;
-                        shadewalker1_Right = true;
-                    }
-                }
-            }
-    if(shadewalker2_Alive) {
-        
-                if (shadewalker2_x >= 440 && shadewalker2_Right) {
-                    shadewalker2_x += 1;
-                    shadeWalker_Sprite.draw(shadewalker2_x, shadewalker2_y, [6,7,8]);
-                    if(shadewalker2_x >= 480) {
-                        shadewalker2_Right = false;
-                        shadewalker2_Left = true;
-                    }
-                }
-                if (shadewalker2_x <= 500 && shadewalker2_Left) {
-                    shadewalker2_x -= 1;
-                    shadeWalker_Sprite.draw(shadewalker2_x, shadewalker2_y, [3,4,5]);
-                    if (shadewalker2_x <= 440) {
-                        shadewalker2_Left = false;
-                        shadewalker2_Right = true;
-                    }
-                }
-            }
+    shadewalker1_Engaged = enemyAggro(shadewalker1_x, shadewalker1_y, shadewalker1_Alive, shadewalker1_Engaged);
+    shadewalker2_Engaged = enemyAggro(shadewalker2_x, shadewalker2_y, shadewalker2_Alive, shadewalker2_Engaged);
+        if (shadewalker1_Alive) {
+            shadewalker1_x = enemyPatrol(shadewalker1_Alive, shadewalker1_x, shadewalker1_y, shadewalker1_Left, shadewalker1_Right, "shadewalker", 210, 130, false);
+            if (shadewalker1_x >= 200 && shadewalker1_Right) { shadewalker1_Right = false; shadewalker1_Left = true; }
+            if (shadewalker1_x <= 130 && shadewalker1_Left) { shadewalker1_Left = false; shadewalker1_Right = true; }
+        }
+        if (shadewalker2_Alive) {
+            shadewalker2_x = enemyPatrol(shadewalker2_Alive, shadewalker2_x, shadewalker2_y, shadewalker2_Left, shadewalker2_Right, "shadewalker", 500, 440, false);
+            if (shadewalker2_x >= 480 && shadewalker2_Right) { shadewalker2_Right = false; shadewalker2_Left = true; }
+            if (shadewalker2_x <= 440 && shadewalker2_Left) { shadewalker2_Left = false; shadewalker2_Right = true; }
+        }
     if (player_coordinates_x >= 315 && player_coordinates_x <= 330 &&
        player_coordinates_y >= 430 && player_coordinates_y <= 450) {
          cancelAnimationFrame(requestID); player_coordinates_x = 460; player_coordinates_y = 330; caveEntrance(); return;
